c01a8dafd99ae24f78e970fac673c32c7159e943
Miguel Madrid Mencía <mimadrid@ucm.es>
2018-02-13 19:55:02 +0100

    Evolution VI mockup:
      - Only one arc
      - Added color gradient selector to the arcs

--- evolution_VI_mockup/js/widgets/NewDemoDrawer.js
+++ evolution_VI_mockup/js/widgets/NewDemoDrawer.js
@@ -14,6 +14,46 @@ import online_tick from '../../icons/online-tick.png';
 import offline_tick from '../../icons/offline-tick.png';
 import close_button from '../../icons/close-button.png';
 
+// https://stackoverflow.com/a/27709336
+function colorStepGradiant (start_color, end_color, percent) {
+   // strip the leading # if it's there
+   start_color = start_color.replace(/^\s*#|\s*$/g, '');
+   end_color = end_color.replace(/^\s*#|\s*$/g, '');
+
+   // convert 3 char codes --> 6, e.g. `E0F` --> `EE00FF`
+   if(start_color.length == 3){
+     start_color = start_color.replace(/(.)/g, '$1$1');
+   }
+
+   if(end_color.length == 3){
+     end_color = end_color.replace(/(.)/g, '$1$1');
+   }
+
+   // get colors
+   var start_red = parseInt(start_color.substr(0, 2), 16),
+       start_green = parseInt(start_color.substr(2, 2), 16),
+       start_blue = parseInt(start_color.substr(4, 2), 16);
+
+   var end_red = parseInt(end_color.substr(0, 2), 16),
+       end_green = parseInt(end_color.substr(2, 2), 16),
+       end_blue = parseInt(end_color.substr(4, 2), 16);
+
+   // calculate new color
+   var diff_red = end_red - start_red;
+   var diff_green = end_green - start_green;
+   var diff_blue = end_blue - start_blue;
+
+   diff_red = ( (diff_red * percent) + start_red ).toString(16).split('.')[0];
+   diff_green = ( (diff_green * percent) + start_green ).toString(16).split('.')[0];
+   diff_blue = ( (diff_blue * percent) + start_blue ).toString(16).split('.')[0];
+
+   // ensure 2 digits by color
+   if( diff_red.length == 1 ) diff_red = '0' + diff_red
+   if( diff_green.length == 1 ) diff_green = '0' + diff_green
+   if( diff_blue.length == 1 ) diff_blue = '0' + diff_blue
+
+   return '#' + diff_red + diff_green + diff_blue;
+ };
 // All the widget drawing functions take as input an element and the
 // fetched data
 var NewDemoDrawer = {
@@ -69,6 +109,10 @@ var NewDemoDrawer = {
 
 
   draw: function(widgetElem, widgetData) {
+    for (let metric of widgetData.metrics) {
+        delete metric.submetrics
+    }
+
     var widgetRoot = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
     widgetRoot.setAttribute('class', 'widget');
     widgetRoot.setAttributeNS('http://www.w3.org/2000/xmlns/', 'xmlns:xlink', 'http://www.w3.org/1999/xlink');
@@ -155,8 +199,8 @@ var NewDemoDrawer = {
       .endAngle(function(d) {
         return d.x1 - 0.005;
       })
-      .innerRadius(function(d) {
-        return d.y0;
+      .innerRadius(function() {
+        return levelSize+1;
       })
       .outerRadius(function(d) {
         return d.y1;
@@ -237,6 +281,17 @@ var NewDemoDrawer = {
         return d.data.empty ? 0 : 1;
       })
       .style('fill', function(d) {
+        //gradient here
+        if (d.data.ticks) {
+          var  ticks_counter = 0
+          var  total_ticks = 0
+
+          for (let tick of d.data.ticks) {
+            if (tick.ticked) ticks_counter++
+            total_ticks++
+          }
+          return colorStepGradiant('#FFFFFF', d.data.color, ticks_counter/total_ticks)
+        }
         return d.data.color;
       })
       .on('mousemove', tooltipFunc)
