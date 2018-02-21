f14e94897d9dd2a3f90c584f58cee354fc588179
Miguel Madrid Mencía <miguel.madrid@bsc.es>
2018-02-21 15:57:14 +0100

    Evolution VIb:
      - Added badge

---
+++ evolution_VIb_mockup/README.md
@@ -0,0 +1,11 @@
+# Instructions to build the widget's development version
+```
+yarn install
+yarn dev
+```
+
+# Instructions to build the widget's distribution version
+```
+yarn install
+yarn dist
+```
---
+++ evolution_VIb_mockup/webpack.config.js
@@ -0,0 +1,101 @@
+var webpack = require('webpack')
+var path = require('path');
+var merge = require('webpack-merge');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+var DEPLOYMENT = process.env.WEBPACK_DEPLOYMENT || 'dev'
+
+var common = {
+  entry: './js/OpEB-widgets.js',
+  module: {
+    loaders: [
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          //'url-loader?limit=130000',
+          'url-loader',
+          {
+            loader: 'img-loader',
+            options: {
+              enabled: DEPLOYMENT === 'dist',
+              gifsicle: {
+                interlaced: false
+              },
+              mozjpeg: {
+                progressive: true,
+                arithmetic: false
+              },
+              optipng: false, // disabled
+              pngquant: {
+                floyd: 0.5,
+                speed: 2
+              },
+              svgo: {
+                plugins: [
+                  { removeTitle: true },
+                  { convertPathData: false }
+                ]
+              }
+            }
+          }
+        ]
+      }
+    ],
+  },
+};
+
+if(DEPLOYMENT === 'dev') {
+  module.exports = merge(common, {
+    output: {
+      filename: 'OpEB-widgets.js',
+      path: __dirname + '/build'
+    },
+    devtool: 'eval-source-map',
+    devServer: {
+      inline: true
+    },
+    module: {
+      // loaders will get concatenated!
+      loaders: [
+        {
+          test: /\.css$/,
+          use: [
+            { loader: 'style-loader' },
+            { loader: 'css-loader',
+              options: { minimize: false }
+            },
+          ]
+        }
+      ],
+    },
+  });
+}
+
+if(DEPLOYMENT === 'dist') {
+  module.exports = merge(common, {
+    output: {
+      filename: 'OpEB-widgets.js',
+      path: __dirname + '/dist'
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.css$/,
+          use: [
+            { loader: 'style-loader' },
+            { loader: 'css-loader',
+              options: { minimize: true }
+            }
+          ]
+        },
+      ]
+    },
+    plugins: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          compress: { warnings: true },
+        }
+      })
+    ]
+  });
+}
