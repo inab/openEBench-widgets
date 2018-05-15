3aeaa6c9902b033be4731330de873fd16e95e52c
Miguel Madrid Mencía <miguel.madrid@bsc.es>
2018-05-15 15:19:02 +0200

    Update uptime to the last version

--- js/widgets/Widget.js
+++ js/widgets/Widget.js
@@ -299,7 +299,7 @@ var WidgetDrawer = {
       .attr('id', 'tooltip_uptime-' + widgetIdCss)
       .attr('class', 'tooltip');
 
-    var uptime_url = widgetData.name.replace('metrics/', 'rest/homepage/');
+    var uptime_url = widgetData.name.replace('https://openebench.bsc.es/monitor/metrics/', '');
 
     tooltip_uptime.html('<div id="close_icon-uptime-' + widgetIdCss + '" style="float: right"></div></br><div data-id="' + widgetIdCss + '" data-xaxis="true" data-w="400" data-h="200" data-url="' + uptime_url + '" class="opebuptime" ></div>');
     tooltip_uptime.style('opacity', 1);
